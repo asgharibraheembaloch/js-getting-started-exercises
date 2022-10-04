c0c077d7ceaed4bc93c30325407779a6a985db1c
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-10-04 17:51:48 +0500

    Update stripping-names.js

--- Stripping_Names/stripping-names.js
+++ Stripping_Names/stripping-names.js
@@ -1,4 +1,7 @@
 const personName = "\tAsghar\nIbraheem\nBaloch\t"
+
+// before stripping white space around person name
 console.log(personName)
 
+// after stripping white space around person name
 console.log(personName.trim())
