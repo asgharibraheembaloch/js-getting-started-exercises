9e5352d6298fbae571fd6b66cec3c2bc6abe6595
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-10-04 18:07:32 +0500

    challenge completed

---
+++ Favorite_Number/favorite-number.js
@@ -0,0 +1,10 @@
+/* 
+    challange
+Store your favorite number in a variable. Then, using that variable, 
+create a message that reveals your favorite number. Print that message.
+*/
+
+const favouriteNumber = 1
+
+// message revealing my favourite number
+console.log(`my favourite number is ${favouriteNumber}`)
---
+++ Number_Eight/number-eight.js
@@ -0,0 +1,20 @@
+/*
+    #Challange#
+Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+Be sure to enclose your operations in print statements to see the results.
+You should create four lines that look like this:
+console.log(5 + 3)
+*/
+
+// addition
+console.log(5 + 3)
+
+// subtraction
+console.log(10-2)
+
+// multipication
+console.log(4 * 2)
+
+// division
+console.log(16 / 2)
+
