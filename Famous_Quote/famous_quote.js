31e32a2335b7a4b8913c8f64ef58a4d017ad9658
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-10-04 17:22:27 +0500

    updated famous quote file

--- Famous_Quote/famous_quote.js
+++ Famous_Quote/famous_quote.js
@@ -5,10 +5,4 @@ Your output should look something like the following, including the quotation ma
 Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 */
 
-const Quote = {author:'Mother Teresa', Quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier."}
-function famousQuote(quote){
-    const {author, Quote} = quote
-    return (`${author} once said, "${Quote}"`)
-}
-
-console.log(famousQuote(Quote))
+console.log('Mother Teresa once said,','"Spread love everywhere you go. Let no one ever come to you without leaving happier."');
---
+++ Famous_Quote_2/famous-quote-2.js
@@ -0,0 +1,10 @@
+/* 
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called 
+famous_person. Then compose your message and store it in a new variable called message. Print your message. 
+*/
+
+const famous_person = 'Benjamin Franklin'
+
+let message = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+
+console.log(`${famous_person} once said, "${message}"`)
