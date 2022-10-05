9c0356cb42af12d2e15d9c0549f372e5aa77ef23
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-10-05 16:54:07 +0500

    updated changing guest list challange

--- Changing_Guest_List/changing-guest-list.js
+++ Changing_Guest_List/changing-guest-list.js
@@ -1,5 +1,8 @@
 /* You just heard that one of your guests can’t make the dinner, so you need to send out a new set of 
 invitations. You’ll have to think of someone else to invite.
+• Add a print statement at the end of your program stating the name of the guest who can’t make it.
+• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+• Print a second set of invitation messages, one for each person who is still in your list.
  */
 
 let favouritePerson = ["zia khan", "zeesha hanif", "danial nagori"]
@@ -13,10 +16,11 @@ for (let i = 0; i < favouritePerson.length; i++){
     if (reply[favouritePerson[i]] === "available"){
         invitation.push(favouritePerson[i])
     }
-    else if (reply[favouritePerson[i]] === "not available"){
+    else if (reply[favouritePerson[i]] === "not available"){        
+        console.log(`${favouritePerson[i]} is not avaialble for dinner`)
         // ahmad is always available
-        invitation.push("ahmad")
+        favouritePerson[i] = "ahmad"
     }
 }
 
-console.log(invitation)
+console.log(favouritePerson)
